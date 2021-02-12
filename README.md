058e0be43a9c6c9bac770bac28961739573d8d2d
KooKu <kooku@kakao.com>
2021-02-13 08:26:16 +0900

    env: scaffolding

---
+++ README.md
@@ -0,0 +1,1 @@
+# Next.js + Emotion Template
---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Head from 'next/head';
+
+const Home: React.FC = () => {
+  return (
+    <>
+      <Head>
+        <title>처음 만나는 건물재테크, 카사 - Kasa</title>
+      </Head>
+
+      <div>MAIN</div>
+    </>
+  );
+};
+
+export default Home;
